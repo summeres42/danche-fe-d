50a706e7d10645f1b2e1aaf7beea84f70865e3c8
ayrikiya <2916709093@qq.com>
2020-03-15 19:37:27 +0800

    first

---
+++ precache-manifest.f3bf01aae38486f65830772e5764aed6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ea2799449a8b13507372e29a589987",
+    "url": "/index.html"
+  },
+  {
+    "revision": "408d7090d78ee3d19a66",
+    "url": "/static/css/2.cc21a976.chunk.css"
+  },
+  {
+    "revision": "7ec928756b3ac82346c9",
+    "url": "/static/css/main.a51d9b05.chunk.css"
+  },
+  {
+    "revision": "408d7090d78ee3d19a66",
+    "url": "/static/js/2.66c97795.chunk.js"
+  },
+  {
+    "revision": "7ec928756b3ac82346c9",
+    "url": "/static/js/main.840fa0f7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "94b27b7cdfe5ab21cee2793614e1aea1",
+    "url": "/static/media/backgound.94b27b7c.jpg"
+  }
+]);
