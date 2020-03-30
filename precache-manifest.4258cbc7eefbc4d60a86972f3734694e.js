d0f76af25d3c630ec01bf3f321baf34155ddfd9b
ayrikiya <2916709093@qq.com>
2020-03-30 16:47:22 +0800

    修改axios拦截

---
+++ precache-manifest.4258cbc7eefbc4d60a86972f3734694e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc2b5943190a24a72c33093144c53ad8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2855f574f1512cf37b07",
+    "url": "/static/css/2.cc21a976.chunk.css"
+  },
+  {
+    "revision": "85879d4d68179c253849",
+    "url": "/static/css/main.a51d9b05.chunk.css"
+  },
+  {
+    "revision": "2855f574f1512cf37b07",
+    "url": "/static/js/2.5fe01829.chunk.js"
+  },
+  {
+    "revision": "85879d4d68179c253849",
+    "url": "/static/js/main.644f28ad.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "94b27b7cdfe5ab21cee2793614e1aea1",
+    "url": "/static/media/backgound.94b27b7c.jpg"
+  }
+]);
